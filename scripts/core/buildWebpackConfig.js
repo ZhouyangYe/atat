c1554c252615f84aabfe91c02744a2ac6c4ee79a
Zhouyang Ye <zye0821@gmail.com>
2022-06-24 22:13:22 +0800

    feat: move webpack configs of prod and dev env to webpack_common

--- scripts/core/buildWebpackConfig.js
+++ scripts/core/buildWebpackConfig.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const webpackCommon = require(path.resolve(__dirname, '../../webpack_common'));
@@ -48,20 +46,7 @@ const buildCommonConfig = (config, name) => {
 
 // get web pack config for development mode
 const buildDevConfig = (config, name) => {
-  const completeConfig = merge(webpackCommon, config);
-  completeConfig.mode = WEBPACK_MODE.DEV;
-
-  completeConfig.devtool = 'source-map'; // add sourcemap for dev mode
-  completeConfig.module.rules.forEach(rule => {
-    if (rule.id === 'css-loaders') { // enable source map for css loaders
-      if (rule.use) rule.use.forEach(loader => {
-        if (loader.options) {
-          loader.options.sourceMap = true;
-        }
-      });
-    }
-    delete rule.id;
-  });
+  const completeConfig = merge(webpackCommon.common, webpackCommon[WEBPACK_MODE.DEV], config);
 
   // use local code for dev
   if (config.externals) {
@@ -73,32 +58,7 @@ const buildDevConfig = (config, name) => {
 
 // get web pack config for production mode
 const buildProdConfig = (config, name) => {
-  const completeConfig = merge(webpackCommon, config);
-  completeConfig.mode = WEBPACK_MODE.PROD;
-
-  const cssMinifyPlugin = new OptimizeCSSAssetsPlugin({}); // minify css
-
-  const jsMinifyPlugin = new TerserPlugin({ // uglify js
-    parallel: true,
-    terserOptions: {},
-  });
-
-  if (completeConfig.optimization) {
-    if (completeConfig.optimization.minimizer) {
-      completeConfig.optimization.minimizer.push(cssMinifyPlugin, jsMinifyPlugin);
-    } else {
-      completeConfig.optimization.minimizer = [cssMinifyPlugin, jsMinifyPlugin];
-    }
-  } else {
-    completeConfig.optimization = {
-      minimize: true,
-      minimizer: [cssMinifyPlugin, jsMinifyPlugin],
-    };
-  }
-
-  completeConfig.module.rules.forEach(rule => { // remove custom property in case webpack throws errors
-    delete rule.id;
-  });
+  const completeConfig = merge(webpackCommon.common, webpackCommon[WEBPACK_MODE.PROD], config);
 
   return buildCommonConfig(completeConfig, name);
 };
--- webpack_common/index.js
+++ webpack_common/index.js
@@ -1,39 +1,95 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { WEBPACK_MODE } = require('../scripts/enum');
+
+const cssMinifyPlugin = new OptimizeCSSAssetsPlugin({}); // minify css
+
+const jsMinifyPlugin = new TerserPlugin({ // uglify js
+  parallel: true,
+  terserOptions: {},
+});
 
 module.exports = {
-  resolve: {
-    extensions: ['.js', '.ts', '.json'], // import file extensions
-    modules: [ // where to find the imported file
-      path.resolve(__dirname, '../node_modules'),
-    ],
+  common: {
+    resolve: {
+      extensions: ['.js', '.ts', '.json'], // import file extensions
+      modules: [ // where to find the imported file
+        path.resolve(__dirname, '../node_modules'),
+      ],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          loader: 'ts-loader', // compile typescript
+          exclude: /node_modules/,
+        },
+      ],
+    },
   },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        loader: 'ts-loader', // compile typescript
-        exclude: /node_modules/,
-      },
-      {
-        id: 'css-loaders',
-        test: /\.(less|css)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,  // output css to file
-          },
-          {
-            loader: 'css-loader', // import css in javascript
-            options: {
-              url: false,
+
+  [WEBPACK_MODE.PROD]: {
+    mode: WEBPACK_MODE.PROD,
+    module: {
+      rules: [
+        {
+          test: /\.(less|css)$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,  // output css to file
             },
-          },
-          {
-            loader: 'less-loader', // compile less
-            options: {},
-          },
-        ],
-      },
-    ],
+            {
+              loader: 'css-loader', // import css in javascript
+              options: {
+                url: false,
+              },
+            },
+            {
+              loader: 'less-loader', // compile less
+              options: {},
+            },
+          ],
+        },
+      ],
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        cssMinifyPlugin,
+        jsMinifyPlugin,
+      ],
+    }
+  },
+
+  [WEBPACK_MODE.DEV]: {
+    mode: WEBPACK_MODE.DEV,
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(less|css)$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,  // output css to file
+            },
+            {
+              loader: 'css-loader', // import css in javascript
+              options: {
+                url: false,
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'less-loader', // compile less
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
   },
 };
