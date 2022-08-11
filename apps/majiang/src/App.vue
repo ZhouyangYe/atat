dbdc83d1944fb4e100b7641ee95b9ee721961098
Zhouyang Ye <zye0821@gmail.com>
2022-08-11 22:36:23 +0800

    feat: handle window resize

--- apps/majiang/src/App.vue
+++ apps/majiang/src/App.vue
@@ -7,6 +7,8 @@ body {
   #app {
     width: 100vw;
     height: 100vh;
+    overflow: hidden;
+    position: relative;
   }
 }
 </style>
--- apps/majiang/src/Table/TableComponent.vue
+++ apps/majiang/src/Table/TableComponent.vue
@@ -1,11 +1,14 @@
 <style lang="less">
 .table {
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: 0 auto;
   perspective: 1400px;
   perspective-origin: 50% 0%;
   transform-style: preserve-3d;
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
+  width: 1920px;
+  height: 972px;
 
   .slot {
     position: absolute;
@@ -35,12 +38,12 @@
     left: calc(50% - 35px);
 
     &>div {
-      @width: 2500px;
+      @width: 3000px;
       width: @width;
       height: @width;
       box-sizing: border-box;
       background-color: rgb(109, 36, 36);
-      transform: translate3d(-1250px, -1250px, -760px) rotateX(90deg);
+      transform: translate3d(-1500px, -1400px, -1160px) rotateX(90deg);
       border: 80px solid rgb(153, 116, 24);
     }
   }
@@ -48,7 +51,7 @@
 </style>
 
 <template>
-  <div class="table">
+  <div class="table" :style="{ transform: `scale(${scale})`, top: `${top}px` }">
     <div class="tabletop">
       <div />
     </div>
@@ -129,7 +132,24 @@ export default defineComponent({
     return {
       msg: 'Hello Vue',
       type: TileType,
+      scale: 1,
+      top: 0,
     };
   },
+  created() {
+    this.handleResize();
+    window.addEventListener('resize', this.handleResize, false);
+  },
+  unmounted() {
+    window.removeEventListener('resize', this.handleResize, false);
+  },
+  methods: {
+    handleResize() {
+      const width = window.innerWidth, height = window.innerHeight;
+      const s1 = width / 1920, s2 = height / 972;
+      this.scale = Math.min(s1, s2);
+      this.top = (height - 972) / 2;
+    },
+  }
 });
 </script>
